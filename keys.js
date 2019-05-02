250f2f20e216df3aad223166e27dc7272597b52f
Veronique Conus <veronique@Veroniques-MacBook-Pro.local>
2019-05-02 00:16:28 -0400

    updated variables in app js

--- keys.js
+++ keys.js
@@ -1,6 +1,12 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: '8b39d6f3d18f4c9fa98058cd7b3907e2',//process.env.SPOTIFY_ID,
+  secret: '88636b8aa31c461082b750506e068a43', //process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+  api_key: 'cb684d38'
+};
+
+//waiting on Band API
--- liri.js
+++ liri.js
@@ -1,11 +1,15 @@
-//<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-//search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
-
+var fs = require("fs");
+var moment = require("moment");
 var Spotify = require('node-spotify-api');
-var  Env = require('dotenv').config();
+var omdbRequest = require("request");
 var keys = require("./keys.js");
+var  Env = require('dotenv').config();
 var spotify = new Spotify(keys.spotify);
  
+var inputType = process.argv[2];
+var entries = process.argv.slice(3);
+var request = entries.join(" ");
+
 spotify
     .search({ type: 'track', query: 'All the Small Things' })
     .then(function(response) {
@@ -24,12 +28,12 @@ spotify
     console.error('Error occurred: ' + err)
   });
 
-const db = require('db')
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-});
+// const db = require('db')
+// db.connect({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS
+// });
  
 const result = dotenv.config() 
     if (result.error) {
